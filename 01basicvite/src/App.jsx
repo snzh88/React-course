b6f9f7e0189492d1b18537f38ab77e17a0e8e5bb
ssaannzzhhiikk <omarkhanovsanzh@gmail.com>
2025-04-23 23:02:00 +0500

    Lesson 1. Comparison between CRA and Vite, understanding core file structure and flow,

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube.jsx"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Vite react app</h1>
+      <h1>Vite react app</h1>
+      <Youtube />
+    </div>
+  )
+}
+
+export default App
