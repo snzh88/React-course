0570d05fdec0075529f7383788ad1cfe87a04013
Sanzhar <omarkhanovsanzh@gmail.com>
2025-05-30 00:02:58 +0500

    Fixed

--- 09themeswitcher/vite.config.js
+++ 09themeswitcher/vite.config.js
@@ -6,8 +6,6 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss({
-      darkMode: "class",
-    }),
+    tailwindcss(),
   ],
 })
